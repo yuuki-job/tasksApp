377c856cd39814c23689b0d024a74cf6a89546b0
yuuki <ty0427s8145460@icloud.com>
2020-09-06 23:51:11 +0900

    ＋ボタンを押すと次の画面に遷移する機能を追加

--- taskApp/taskApp1/Controller/ViewController.swift
+++ taskApp/taskApp1/Controller/ViewController.swift
@@ -26,7 +26,22 @@ class ViewController: UIViewController {
         tableView.register(UINib(nibName: "CustomCell", bundle: nil), forCellReuseIdentifier: "Cell")
         
         
+        let button:UIButton = UIButton(frame: CGRect(x: 300, y: 650, width: 40, height: 40))
+        button.backgroundColor = .blue
+        button.setTitle("＋", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 20
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        
+        self.view.addSubview(button)
+        
     }
+    
+    @objc func didTapButton() {
+        let aptVC = self.storyboard?.instantiateViewController(withIdentifier: "task") as! apendTaskViewController
+        self.navigationController?.pushViewController(aptVC, animated: true)
+    }
+
 }
 extension ViewController:UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
---
+++ taskApp/taskApp1/Controller/apendTaskViewController.swift
@@ -0,0 +1,30 @@
+//
+//  apendTaskViewController.swift
+//  taskApp1
+//
+//  Created by 徳永勇希 on 2020/09/06.
+//  Copyright © 2020 yuuki. All rights reserved.
+//
+
+import UIKit
+
+class apendTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
