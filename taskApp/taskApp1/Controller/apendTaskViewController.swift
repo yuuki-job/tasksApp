cb4e0d10e886f1e5036d06e89b23cec22f9f65c9
yuuki <ty0427s8145460@icloud.com>
2020-09-07 14:36:14 +0900

    textFieldに入力できるよにした

--- taskApp/taskApp1/Controller/apendTaskViewController.swift
+++ taskApp/taskApp1/Controller/apendTaskViewController.swift
@@ -8,23 +8,51 @@
 
 import UIKit
 
-class apendTaskViewController: UIViewController {
+class apendTaskViewController: UIViewController, UIPickerViewDataSource {
+    
+    
+    
+    
+
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var dateTextField: UITextField!
+    
+    var datepickerView = UIDatePicker()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       createPickerView()
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 1
     }
-    */
-
+    
+    
+    func createPickerView() {
+        //datepickerView.delegate = self
+        dateTextField.inputView = datepickerView
+        // toolbar
+        let toolbar = UIToolbar()
+        toolbar.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 44)
+        let doneButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(apendTaskViewController.donePicker))
+        toolbar.setItems([doneButtonItem], animated: true)
+        dateTextField.inputAccessoryView = toolbar
+        
+    }
+    
+    @objc func donePicker() {
+        dateTextField.endEditing(true)
+    }
+    
+    
+    @IBAction func taskApendButton(_ sender: Any) {
+        
+        
+    }
+    
 }
